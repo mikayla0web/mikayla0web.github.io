12d3c8cac3559c6e4eb6ad514af66c3994ccde5c
Mikayla Weber <mikayla.gramza@gmail.com>
2025-05-17 17:47:27 +0000

    dfdfd

--- main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.miracosta.cs112.finalproject.finalproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-    opens edu.miracosta.cs112.finalproject.finalproject to javafx.fxml;
-    exports edu.miracosta.cs112.finalproject.finalproject;
-}
